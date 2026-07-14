ebff9ba0453cc5b8f5e24c6c084d4da68b022a5d
WolfRichie <brtem1443@gmail.com>
2026-07-14 20:58:34 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from code.Proxy import PyJinnProxy, PyJinnProxyMeta
 PyJinnProxyMeta.bind_script("library.pyj")
 
 if TYPE_CHECKING:
-  from code.type_checking import ContainerHelper, MappingsHelper, FishingHelper
+  from code.type_checking import ContainerHelper, MappingsHelper, FishingHelper, ItemsHelper
 else:
   class MappingsHelper(PyJinnProxy):
     pass
@@ -13,4 +13,7 @@ else:
     pass
 
   class FishingHelper(PyJinnProxy):
+    pass
+
+  class ItemsHelper(PyJinnProxy):
     pass

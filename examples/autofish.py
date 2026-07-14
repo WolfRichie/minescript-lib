764042db74d8eb68b1ca4d9dce5e004a53290732
WolfRichie <brtem1443@gmail.com>
2026-07-14 14:14:02 +0200

    Create autofish.py

---
+++ examples/autofish.py
@@ -0,0 +1,25 @@
+from main import ContainerHelper, MappingsHelper, FishingHelper
+from time import sleep
+
+while True:
+		has_rod = FishingHelper.has_fishing_rod()
+		casted = FishingHelper.is_casted()
+		biting = FishingHelper.is_biting() if casted else False
+
+		if not has_rod:
+			print("waiting: hold a fishing rod in main or off hand")
+			sleep(1.0)
+			continue
+
+		if not casted:
+			print("action: casting rod")
+			result = FishingHelper.cast()
+			continue
+
+		if biting:
+			print("action: bite detected")
+			reel_result = FishingHelper.use_rod()
+			sleep(0.25)
+			print("action: recasting")
+			recast_result = FishingHelper.use_rod()
+			sleep(1.0)
